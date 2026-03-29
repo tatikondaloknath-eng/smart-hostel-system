54d57b0d369515510eb68b0a928ccc8a687506da
tatikondaloknath-eng <tatikondaloknath@gmail.com>
2026-03-29 09:57:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -252,6 +252,6 @@ if __name__ == '__main__':
 #     conn.close()
 #     return jsonify({"success": True})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(host='0.0.0.0', port=port)
